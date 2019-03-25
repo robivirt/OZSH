610f6f97563e3245ce44c6576209a0311252cd5e
HoBocTu <43210583+HoBocTu@users.noreply.github.com>
2019-03-25 10:17:46 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+int main() {
+    int count = 0;
+    for (int a = 1; a < 10; a++) {
+        for (int b = 0; b < 10; b++) {
+            for (int c = 0; c < 10; c++) {
+                for (int d = 0; d < 10; d++) {
+                    if ((a == b && a != c && a != d && c != d) || (a == c && a != b && a != d && b != d) || (a == d && a != c && a != b && c != b)  || (b == c && b != a && b != d && a != d) || (b == d && b != a && b != c && a != c) || (c == d && c != a && c != b && a != b)) count++;
+                }
+            }
+        }
+    }
+    std::cout << count;
+    return 0;
+}
