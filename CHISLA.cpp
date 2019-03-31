bee6cbb028358a56fd8914f25365f960881da5d4
robivirt <42174893+robivirt@users.noreply.github.com>
2019-03-31 19:22:19 +0500

    Update CHISLA.cpp

--- CHISLA.cpp
+++ CHISLA.cpp
@@ -11,7 +11,8 @@ int main() {
         for (int b = 0; b < 10; b++) {
             for (int c = 0; c < 10; c++) {
                 for (int d = 0; d < 10; d++) {
-                    if ((a == b && a != c && a != d && c != d) || (a == c && a != b && a != d && b != d) || (a == d && a != c && a != b && c != b)  || (b == c && b != a && b != d && a != d) || (b == d && b != a && b != c && a != c) || (c == d && c != a && c != b && a != b)) count++;
+                    //if ((a == b && a != c && a != d && c != d) || (a == c && a != b && a != d && b != d) || (a == d && a != c && a != b && c != b)  || (b == c && b != a && b != d && a != d) || (b == d && b != a && b != c && a != c) || (c == d && c != a && c != b && a != b)) count++;
+                    if(a == b || b == c || c == a) count++;                    
                 }
             }
         }
